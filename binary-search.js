92ccd320d509807350e734e9420dab77884f2cb8
Vladislav V <Vladislav V>
2017-03-20 10:58:23 -0400

    Simple binary search implementation

---
+++ binary-search.js
@@ -0,0 +1,26 @@
+function binarySearch(numbers,element){
+  if(numbers.length==0){
+    return false;
+  }else if(numbers.length==1){
+    return numbers[0] === element;
+  }else{
+    var medIndex = Math.round(numbers.length/2);
+    var medValue = numbers[medIndex];
+    if(element === medValue){
+      return true;
+    }else{
+      if(element < medValue){
+        return binarySearch(numbers.slice(0,medIndex),element);
+      }else{
+        return binarySearch(numbers.slice(medIndex),element);
+      }
+    }
+  }
+}
+
+var numbers = [1,3,5,7,9];
+
+for(var i=0;i<=10;i++){
+  var exists = binarySearch(numbers,i);
+  console.log(i+" exists in "+numbers+" -> "+exists);
+}
